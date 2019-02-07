d8f8a8ae4e434c47ca0b378fca2eea8f7258e112
Toni Mueller <mueller-toni@ipfdd.de>
2019-02-07 20:09:55 +0100

    add output streams for the bfm file

--- include/LeMonADE/feature/FeatureConnectionSc.h
+++ include/LeMonADE/feature/FeatureConnectionSc.h
@@ -38,6 +38,9 @@ along with LeMonADE.  If not, see <http://www.gnu.org/licenses/>.
 #include <LeMonADE/updater/moves/MoveAddMonomerBcc.h>
 #include <LeMonADE/updater/moves/MoveAddMonomerSc.h>
 #include <LeMonADE/utility/Lattice.h>
+#include <LeMonADE/io/AbstractRead.h>
+#include <LeMonADE/io/AbstractWrite.h>
+#include <LeMonADE/io/FileImport.h>
 /**
  * @class MonomerReactivity
  * @brief set the monomer reactive or unreactive 
@@ -56,12 +59,29 @@ class MonomerReactivity
 	//! Getting the number of maximum possible bonds for the monomer.
 	uint32_t getNMaxBonds() const {return nMaxBonds;};
 
+	MonomerReactivity getReactivity(){return *this; }
+	void setReactivity(const MonomerReactivity& react)
+	{
+	  reactivity = react.IsReactive();
+	  nMaxBonds = react.getNMaxBonds();
+	}
+	bool operator == (const MonomerReactivity &react)
+	{
+	  if ( react.getNMaxBonds() == nMaxBonds  ) return false;
+	  if ( react.IsReactive()   == reactivity ) return false;
+	  return true;
+	}
+	bool operator!= (const MonomerReactivity &react) 
+	{ 
+	  return !(*this == react);
+	}
 	/**
 	 * @brief Setting the reactivity of the monomer with \para reactivity_.
 	 *
 	 * @param reactivity_ either trueor false
 	 */
 	void setReactive(bool reactivity_){ reactivity=reactivity_;}
+	
 	/**
 	 * @brief Setting the maximum possible bonds of the monomer with \para nMaxBonds_.
 	 *
@@ -76,6 +96,76 @@ private:
      uint32_t nMaxBonds;
 };
 
+  /**
+  * @brief \b Stream \b Out \b operator of the MonomerReactivity
+  *
+  * @details Streams out the elements chain ID, number of labels and the label 
+  * 	     ID  of the label separated by space
+  *
+  * @param stream output-stream
+  * @param label object of class MonomerReactivity
+  * @return output-stream
+  **/
+  std::ostream& operator<< (std::ostream& stream, const MonomerReactivity & Reactivity)
+  {
+	  stream 	  
+	  << Reactivity.IsReactive() << "/" 
+	  << Reactivity.getNMaxBonds();
+	  return stream;
+  };
+
+  /**
+  * @brief \b Stream \b In \b operator of the MonomerReactivity
+  *
+  * @details Streams in the elements. And setting at first chainID, then NLabels, at last LabelID.
+  *
+  * @param stream input-stream
+  * @param label object of class MonomerReactivity
+  * @return input-stream
+  **/
+  std::istream& operator >> (std::istream& stream, MonomerReactivity & Reactivity)
+  {
+	  int temp;
+	  stream >> temp; Reactivity.setReactive(temp); stream.ignore(1);
+	  stream >> temp; Reactivity.setNMaxBonds(temp); 
+	  return stream;
+  }
+
+
+/*****************************************************************/
+/**
+ * @class ReadReactivity
+ *
+ * @brief Handles BFM-File-Reads \b !reactivity
+ * @tparam IngredientsType Ingredients class storing all system information.
+ */
+template < class IngredientsType>
+class ReadReactivity: public ReadToDestination<IngredientsType>
+{
+public:
+  ReadReactivity(IngredientsType& i):ReadToDestination<IngredientsType>(i){}
+  virtual ~ReadReactivity(){}
+  virtual void execute();
+};
+
+
+/*****************************************************************/
+/**
+ * @class WriteReactivity
+ *
+ * @brief Handles BFM-File-Write \b !reactivity
+ * @tparam IngredientsType Ingredients class storing all system information.
+ **/
+template <class IngredientsType>
+class WriteReactivity:public AbstractWrite<IngredientsType>
+{
+public:
+	//! Only writes \b !reactivity into the header of the bfm-file.
+  WriteReactivity(const IngredientsType& i)
+    :AbstractWrite<IngredientsType>(i){this->setHeaderOnly(true);}
+  virtual ~WriteReactivity(){}
+  virtual void writeStream(std::ostream& strm);
+};
 /*****************************************************************************/
 /**
  * @file
@@ -104,7 +194,6 @@ public:
 	//constructor
 	FeatureConnectionSc() :latticeFilledUp(false)
 	{lattice.setupLattice();}
-
 	
 	/**
 	 * Returns true if the underlying lattice is synchronized and all excluded volume condition
@@ -128,7 +217,13 @@ public:
 	void setLatticeFilledUp(bool latticeFilledUp) {
 		this->latticeFilledUp = latticeFilledUp;
 	}
+	//! Export the relevant functionality for reading bfm-files to the responsible reader object
+	template<class IngredientsType>
+	void exportRead(FileImport<IngredientsType>& fileReader);
 
+	//! Export the relevant functionality for writing bfm-files to the responsible writer object
+	template<class IngredientsType>
+	void exportWrite(AnalyzerWriteBfmFile<IngredientsType>& fileWriter) const;
 	//! check move for basic move - always true
 	template<class IngredientsType>
 	bool checkMove(const IngredientsType& ingredients, const MoveBase& move) const{ return true;};
@@ -194,6 +289,38 @@ protected:
 
 ///////////////////////////////////////////////////////////////////////////////
 ////////////////////////// member definitions /////////////////////////////////
+/**
+ * @details The function is called by the Ingredients class when an object of type Ingredients
+ * is associated with an object of type FileImport. The export of the Reads is thus
+ * taken care automatically when it becomes necessary.\n
+ * Registered Read-In Commands:
+ * * !reactivity
+ *
+ * @param fileReader File importer for the bfm-file
+ * @param destination List of Feature to write-in from the read values.
+ * @tparam IngredientsType Features used in the system. See Ingredients.
+ **/
+template<class IngredientsType>
+void FeatureConnectionSc::exportRead(FileImport< IngredientsType >& fileReader)
+{
+  fileReader.registerRead("!reactivity",new ReadReactivity<IngredientsType>(fileReader.getDestination()));
+}
+
+
+/**
+ * The function is called by the Ingredients class when an object of type Ingredients
+ * is associated with an object of type AnalyzerWriteBfmFile. The export of the Writes is thus
+ * taken care automatically when it becomes necessary.\n
+ * Registered Write-Out Commands:
+ * * !reactivity
+ *
+ * @param fileWriter File writer for the bfm-file.
+ */
+template<class IngredientsType>
+void FeatureConnectionSc::exportWrite(AnalyzerWriteBfmFile< IngredientsType >& fileWriter) const
+{
+  fileWriter.registerWrite("!reactivity",new WriteReactivity<IngredientsType>(fileWriter.getIngredients_()));
+}
 
 /******************************************************************************/
 /**
@@ -327,5 +454,151 @@ void FeatureConnectionSc::fillLattice(IngredientsType& ingredients)
 		}
 	}
 	latticeFilledUp=true;
+}
+
+/**
+ * @brief Executes the reading routine to extract \b !attributes.
+ *
+ * @throw <std::runtime_error> attributes and identifier could not be read.
+ **/
+template < class IngredientsType >
+void ReadReactivity<IngredientsType>::execute()
+{
+  //some variables used during reading
+  //counts the number of reactivity lines in the file
+  int nReactiveBlocks=0;
+  int startIndex,stopIndex;
+  MonomerReactivity reactivity;
+  //contains the latest line read from file
+  std::string line;
+  //used to reset the position of the get pointer after processing the command
+  std::streampos previous;
+  //for convenience: get the input stream
+  std::istream& source=this->getInputStream();
+  //for convenience: get the set of monomers
+  typename IngredientsType::molecules_type& molecules=this->getDestination().modifyMolecules();
+
+  //go to next line and save the position of the get pointer into streampos previous
+  getline(source,line);
+  previous=(source).tellg();
+
+  //read and process the lines containing the bond vector definition
+  getline(source,line);
+
+  while(!line.empty() && !((source).fail())){
+
+    //stop at next Read and set the get-pointer to the position before the Read
+    if(this->detectRead(line)){
+      (source).seekg(previous);
+      break;
+    }
+
+    //initialize stringstream with content for ease of processing
+    std::stringstream stream(line);
+
+    //read vector components
+    stream>>startIndex;
+
+    //throw exception, if extraction fails
+    if(stream.fail()){
+      std::stringstream messagestream;
+      messagestream<<"ReadReactivity<IngredientsType>::execute()\n"
+		   <<"Could not read first index in attributes line "<<nReactiveBlocks+1;
+      throw std::runtime_error(messagestream.str());
+    }
+
+    //throw exception, if next character isnt "-"
+    if(!this->findSeparator(stream,'-')){
+
+    	std::stringstream messagestream;
+      messagestream<<"ReadReactivity<IngredientsType>::execute()\n"
+		   <<"Wrong definition of attributes\nCould not find separator \"-\" "
+		   <<"in attribute definition no "<<nReactiveBlocks+1;
+      throw std::runtime_error(messagestream.str());
+
+    }
+
+    //read bond identifier, throw exception if extraction fails
+    stream>>stopIndex;
+
+    //throw exception, if extraction fails
+    if(stream.fail()){
+    	std::stringstream messagestream;
+      messagestream<<"ReadReactivity<IngredientsType>::execute()\n"
+		   <<"Could not read second index in attributes line "<<nReactiveBlocks+1;
+      throw std::runtime_error(messagestream.str());
+    }
+
+    //throw exception, if next character isnt ":"
+    if(!this->findSeparator(stream,':')){
+
+    	std::stringstream messagestream;
+      messagestream<<"ReadReactivity<IngredientsType>::execute()\n"
+		   <<"Wrong definition of attributes\nCould not find separator \":\" "
+		   <<"in attribute definition no "<<nReactiveBlocks+1;
+      throw std::runtime_error(messagestream.str());
+
+    }
+    //read the attribute tag
+    stream>>reactivity;
+    //if extraction worked, save the attributes
+    if(!stream.fail()){
+
+      //save attributes
+      for(int n=startIndex;n<=stopIndex;n++)
+      {
+	//use n-1 as index, because bfm-files start counting indices at 1 (not 0)
+	molecules[n-1].setReactivity(reactivity);
+      }
+      nReactiveBlocks++;
+      getline((source),line);
+
+    }
+    //otherwise throw an exception
+    else{
+
+    	std::stringstream messagestream;
+      messagestream<<"ReadAttributes<IngredientsType>::execute()\n"
+		   <<"could not read attribute in attribute definition no "<<nReactiveBlocks+1;
+      throw std::runtime_error(messagestream.str());
+
+    }
+  }
+}
+
+
+//! Executes the routine to write \b !reactivity.
+template < class IngredientsType>
+void WriteReactivity<IngredientsType>::writeStream(std::ostream& strm)
+{
+  //for all output the indices are increased by one, because the file-format
+  //starts counting indices at 1 (not 0)
+
+  //write bfm command
+  strm<<"!reactivity\n";
+  //get reference to monomers
+  const typename IngredientsType::molecules_type& molecules=this->getSource().getMolecules();
+
+  size_t nMonomers=molecules.size();
+  //reactivity blocks begin with startIndex
+  size_t startIndex=0;
+  //counter varable
+  size_t n=0;
+  //reactivity to be written (updated in loop below)
+  MonomerReactivity reactivity=molecules[0].getReactivity();
+
+  //write reactivity (blockwise)
+  while(n<nMonomers){
+    if(molecules[n].getReactivity()!=reactivity)
+    {
+      strm<<startIndex+1<<"-"<<n<<":"<<reactivity<<std::endl;
+      reactivity=molecules[n].getReactivity();
+      startIndex=n;
+    }
+    n++;
+  }
+  //write final reactivity
+  strm<<startIndex+1<<"-"<<nMonomers<<":"<<reactivity<<std::endl<<std::endl;
+
 }
 #endif
