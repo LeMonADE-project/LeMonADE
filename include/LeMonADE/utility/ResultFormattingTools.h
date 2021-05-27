cc91c8eb991e1c5921f613d4ee63cb6353b6d526
bondoki <bondoki@gmx.de>
2021-05-27 11:35:41 +0200

    * Hotfix for ResultFormattingTools + floating point types (float, double) is printed with maximum precision to avoid rounding errors

--- include/LeMonADE/utility/ResultFormattingTools.h
+++ include/LeMonADE/utility/ResultFormattingTools.h
@@ -35,7 +35,8 @@ along with LeMonADE.  If not, see <http://www.gnu.org/licenses/>.
 #include <fstream>
 #include <map>
 #include <stdexcept>
-
+#include <limits>
+#include <iomanip>
 
 
 /**
@@ -120,6 +121,8 @@ void ResultFormattingTools::writeTable(std::ostream& stream,
 
 	for (size_t row = 0; row < results[0].size(); ++row) {
 		for (size_t column = 0; column < results.size(); ++column) {
+                        stream.precision(std::numeric_limits<typename ResultType::value_type::value_type>::max_digits10);
+                        stream.setf( std::ios::fixed, std:: ios::floatfield );
 			stream << results[column][row] << "\t";
 		}
 		stream << std::endl;
